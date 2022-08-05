eb8f42038c0186abce4f8e5dbdc7baa73f96d2cc
rohit-shah840 <rohitshah1455@gmail.com>
2022-08-05 12:27:23 +0530

    javadsa

---
+++ aa.java
@@ -0,0 +1,13 @@
+public class aa {
+    public static void main(String[] args) {
+        int n = 2;
+        System.out.println(n >>> 1);
+
+        // if((n & 1) != 0){
+        // pow *= x;
+        // }
+
+        // x *= x;
+        // n >>>= 1;
+    }
+}
